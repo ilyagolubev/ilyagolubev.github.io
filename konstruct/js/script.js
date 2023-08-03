7203050bde152e2d18044f85f439c4a405e09364
Ilya Golubev <95286894+ilyagolubev@users.noreply.github.com>
2023-08-03 12:16:35 +0200

    Delete konstruct directory

--- konstruct/js/script.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-$(function () {
-  $('.nav__burger').on('click', function (){
-    $('.nav__list').toggleClass('nav__list--active');
-    $('.nav__burger').toggleClass('nav__burger--active');
-  });
-})
